3ad1f8f4b58a3126de04bf8043b5e32c164b7fba
Satya Nugraha <satyanugrahaarbain@gmail.com>
2016-01-05 15:36:15 +0700

    Interactive geolocation visualisation
    This project conducted for unicef fiji visualization that examined WASH
    facilitation in Elementary School.
    
    Features:
    1. markercluster
    2. tabbed infowindow

---
+++ filter.js
@@ -0,0 +1,9 @@
+// var filterStruct=[	{"Area":"RTE Indicators", "Icon":"orange.png/yellow.png/green.png/red.png/blank.png","Legend":"< 5/5 to 10/10 to 20/>20/All","Indicator":"Age"},
+// 					{"Area":"RTE Indicators", "Icon":"red.png/green.png/yellow.png/blank.png","Legend":"Not met/Met/NA/All","Indicator":"Quantity","Tooltip":"Met: If the water supply produces more than 0.4 l/s"},
+// 					{"Area":"RTE Indicators", "Icon":"red.png/green.png/yellow.png/blank.png","Legend":"Not met/Met/NA/All","Indicator":"Quality","Tooltip":"Met : If Improved water supply based on technology"},
+// 					{"Area":"RTE Indicators", "Icon":"red.png/green.png/yellow.png/blank.png","Legend":"Not met/Met/NA/All","Indicator":"Accessibility","Tooltip":"Met: If less than 15 households access that water supply"},
+// 					{"Area":"RTE Indicators", "Icon":"red.png/green.png/yellow.png/blank.png","Legend":"Not met/Met/NA/All","Indicator":"Reliability","Tooltip":"Met: If water is available atleast 8 months/ year"},
+// 					{"Area":"RTE Indicators", "Icon":"red.png/yellow.png/green.png/blank.png","Legend":"No functionality/Partial functionality/Full functionality/All","Indicator":"Functionality","Tooltip":"No Funtionality: If only 0-2 QQAR conditions are met.Partial :3 QQAR conditions are met . Full : All QQAR conditions met"}
+// 				  ];
+var filterStruct=[	{"Area":"total_three_star_score", "Icon":"green.png/orange.png/red.png/blank.png","Legend":"1/2/3/All","Indicator":"total_three_star_score"},
+					];
